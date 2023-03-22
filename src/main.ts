5e4bca9f027429b2d43583606045b4cf2fcc9762
Jaden <yjp9603@gmail.com>
2023-03-23 05:42:12 +0900

    Feat: CORS 허용 & API 버저닝 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { logger } from './utils/logger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   logger.info(
@@ -10,7 +12,14 @@ async function bootstrap() {
     `====================== MYSQL_HOST Start - ${process.env.MYSQL_HOST} !!======================`,
   );
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.use(cors.default());
+
   await app.listen(3000);
 }
 bootstrap();
