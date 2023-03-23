3276670a40ad2c440508d6ca2c377fc26a5fc0e9
Jaden <yjp9603@gmail.com>
2023-03-23 22:55:29 +0900

    Fix: 테스트

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,20 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath:
-        process.env.NODE_ENV === 'dev' ? '../env/.env.dev' : '../env/.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod', // production 환경일 때는 configModule이 환경변수 파일을 무시하도록.
-      cache: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      // ignoreEnvFile: process.env.NODE_ENV === 'prod', // production 환경일 때는 configModule이 환경변수 파일을 무시하도록.
+      // cache: true,
       // todo: 환경 변수 유효성 검사 joi
     }),
     //todo: 설정파일 분리
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
+      port: 3306,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE_NAME,
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: [],
       synchronize: false,
       logging: true,
     }),
