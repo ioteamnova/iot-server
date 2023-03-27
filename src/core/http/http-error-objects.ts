663690291acf5d0b897bd68c9464e1f069888ed2
Jaden <yjp9603@gmail.com>
2023-03-28 02:23:28 +0900

    Feat: 회원가입(이메일 중복 검사+비밀번호 암호화) & 엔티티에서 객체 생성하도록 리팩토링

---
+++ src/core/http/http-error-objects.ts
@@ -0,0 +1,57 @@
+/**
+ * HTTP error code 관련 상수
+ * */
+
+export interface HttpErrorFormat {
+  errorCode: string;
+  description?: string;
+  message: string;
+}
+
+export const HttpErrorConstants = {
+  UNAUTHORIZED: {
+    errorCode: 'UNAUTHORIZED',
+    message: '로그인이 필요합니다.',
+  } as HttpErrorFormat,
+
+  FORBIDDEN: {
+    errorCode: 'FORBIDDEN',
+    message: '권한이 없습니다.',
+  } as HttpErrorFormat,
+
+  INTERNAL_SERVER_ERROR: {
+    errorCode: 'INTERNAL_SERVER_ERROR',
+    message: '알 수 없는 오류가 발생하였습니다.',
+  } as HttpErrorFormat,
+
+  EXIST_EMAIL: {
+    errorCode: 'EXIST_EMAIL',
+    message: '이미 가입된 이메일입니다.',
+  } as HttpErrorFormat,
+
+  USER_NOT_FOUND: {
+    errorCode: 'USER_NOT_FOUND',
+    message: '회원정보를 찾을 수 없습니다.',
+  } as HttpErrorFormat,
+
+  INVALID_AUTH: {
+    errorCode: 'UNAUTHORIZED',
+    message: '이메일 또는 비밀번호가 올바르지 않습니다.',
+  } as HttpErrorFormat,
+
+  CANNOT_FIND_USER: {
+    errorCode: 'CANNOT_FIND_USER',
+    message: '유저를 찾을 수 없습니다.',
+  } as HttpErrorFormat,
+
+  AUTH_LINK_EXPIRED: {
+    errorCode: 'AUTH_LINK_EXPIRED',
+    message:
+      '이 메일링크는 이미 사용됐거나, 만료되었습니다(24시간 초과). 메일 인증을 다시 진행해주세요.',
+  } as HttpErrorFormat,
+
+  AUTH_TYPE_INVALID: {
+    errorCode: 'AUTH_TYPE_INVALID',
+    message: '이메일 인증 유형이 유효하지 않습니다.',
+  } as HttpErrorFormat,
+};
--- src/domains/user/entities/user.entity.ts
+++ src/domains/user/entities/user.entity.ts
@@ -1,5 +1,7 @@
 import BaseEntity from 'src/core/entity/base.entity';
+import { hashPassword } from 'src/utils/password.utils';
 import { Column, Entity } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Entity()
 export class User extends BaseEntity {
@@ -24,4 +26,15 @@ export class User extends BaseEntity {
 
   @Column()
   agreeWithEmail: boolean;
+
+  static from(dto: CreateUserDto) {
+    const user = new User();
+    user.email = dto.email;
+    user.nickname = dto.nickname;
+    user.profilePath = dto.profilePath;
+    user.isPremium = dto.isPremium;
+    user.agreeWithEmail = dto.agreeWithEmail;
+    user.password = hashPassword(dto.password);
+    return user;
+  }
 }
