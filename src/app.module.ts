f2bab19533a923861d1274de51fa6a78e8e91e81
Jaden <yjp9603@gmail.com>
2023-03-27 14:23:24 +0900

    Fix: 유저 도메인 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
-      // ignoreEnvFile: process.env.NODE_ENV === 'prod', // production 환경일 때는 configModule이 환경변수 파일을 무시하도록.
-      // cache: true,
       // todo: 환경 변수 유효성 검사 joi
     }),
     //todo: 설정파일 분리
@@ -25,6 +24,7 @@ import { AppService } from './app.service';
       synchronize: false,
       logging: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
