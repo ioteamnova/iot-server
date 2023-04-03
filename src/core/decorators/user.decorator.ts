1da57e2a3df02cbb0fa25e6f6200ca0fb8c7d0e3
Jaden <yjp9603@gmail.com>
2023-04-04 01:47:32 +0900

    Feat: 내 정보 조회 & passport를 사용한 strategy & AuthUser 커스텀 데코레이터 생성

---
+++ src/core/decorators/user.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/domains/user/entities/user.entity';
+
+export const AuthUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): User => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user as User;
+  },
+);
+
+export default AuthUser;
--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -1,6 +1,7 @@
+import { AuthUser } from './../../core/decorators/user.decorator';
 import { AuthService } from './auth.service';
 import { SwaggerTag } from './../../core/swagger/api-tags';
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import {
   ApiBody,
   ApiCreatedResponse,
@@ -10,6 +11,8 @@ import {
 } from '@nestjs/swagger';
 import { logger } from 'src/utils/logger';
 import { LoginUserDto } from './dtos/login-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '../user/entities/user.entity';
 
 @ApiTags(SwaggerTag.AUTH)
 @Controller('/auth')
@@ -19,20 +22,22 @@ export class AuthController {
   @ApiOperation({
     summary: '로그인',
     description: `로그인을 한다. 응답은 token값을 반환한다.
-    - 이메일/비밀번호로 로그인을 시도하여, 엑세스토큰을 발급한다.
-    - 엑세스토큰을 생성하는(create) 행위이기 때문에, POST 로 정의한다.`,
+- 이메일/비밀번호로 로그인을 시도하여, 엑세스토큰을 발급한다.
+- 엑세스토큰을 생성하는(create) 행위이기 때문에, POST 로 정의한다.`,
   })
   @ApiBody({
     type: LoginUserDto,
   })
   @ApiCreatedResponse({ description: '로그인 성공, 토큰 값 발급' })
   @Post()
-  async login(
-    @Res() res,
-    @Body() dto: LoginUserDto,
-  ): Promise<{ accessToken: string }> {
+  async login(@Res() res, @Body() dto: LoginUserDto) {
     const token = await this.authService.login(dto);
-    logger.info('token::', token);
     return res.status(201).send(token);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard('jwt'))
+  async test(@Req() req, @AuthUser() user: User) {
+    console.log('user::', user);
   }
 }
