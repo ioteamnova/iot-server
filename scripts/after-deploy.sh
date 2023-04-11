df4dbe78ea3d80c9e4c2afbbdd1c9fb4c1047740
Jaden <yjp9603@gmail.com>
2023-04-11 19:04:52 +0900

    Fix: 배포 후 실행할 스크립트 파일 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,7 +3,7 @@ REPOSITORY=/home/ubuntu/iot-server/build
 
 cd $REPOSITORY
 
-sudo /usr/bin/npm i
-sudo /usr/bin/pm2 kill
-sudo /usr/bin/pm2 start dist/main.js
+npm i
+pm2 kill
+pm2 start dist/main.js
 
