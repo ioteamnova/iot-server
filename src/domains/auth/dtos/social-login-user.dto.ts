412d5c6bfffda23bdd3097ebd33568629e677ac8
Jaden <yjp9603@gmail.com>
2023-04-13 19:20:33 +0900

    Feat: 카카오 소셜 로그인 추가

--- src/domains/auth/dtos/social-login-user.dto.ts
+++ src/domains/auth/dtos/social-login-user.dto.ts
@@ -4,8 +4,8 @@ import { SocialMethodType } from '../helpers/constants';
 
 export class SocialLoginUserDto {
   @ApiProperty({
-    description: '인가코드',
-    default: 'asdf1234#',
+    description: '액세스 토큰',
+    default: 'W2uID5fWO5NllVWLKMWZvQPo0W_F2FZbEeilIiVMCinI2gAAAYd0M97K',
     required: true,
   })
   @IsNotEmpty()
