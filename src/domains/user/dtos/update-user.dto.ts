30e52252f1856352f0f61c2b37d619d8e03a66ef
Jaden <yjp9603@gmail.com>
2023-04-19 21:24:07 +0900

    Fix: 유저 업데이트 dto 스웨거 표시되지 않도록 수정

--- src/domains/user/dtos/update-user.dto.ts
+++ src/domains/user/dtos/update-user.dto.ts
@@ -3,11 +3,6 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiProperty({
-    description: '프로필 이미지 url',
-    default: null,
-    required: false,
-  })
   @IsString()
   @IsOptional()
   profilePath: string;
