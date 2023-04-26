6de80f5eaa72153315e0e098e5316e713726c640
Jaden <yjp9603@gmail.com>
2023-04-26 19:36:10 +0900

    Fix: 다이어리 목록 조회시 대표이미지와 생성일 추가

--- src/domains/diary/dtos/diary-detail-dto.ts
+++ src/domains/diary/dtos/diary-detail-dto.ts
@@ -1,13 +1,9 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { CreateDiaryDto } from './create-diary.dto';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { DiaryListDto } from './diary-list.dto';
 
-export class DiaryDetailDto extends PartialType(CreateDiaryDto) {
-  @ApiProperty({
-    description: '다이어리 인덱스',
-    default: 1,
-  })
-  idx: number;
-
+export class DiaryDetailDto extends OmitType(DiaryListDto, [
+  'imagePath',
+] as const) {
   @ApiProperty({
     description: '다이어리 이미지 url',
     default: ['https://image1', 'https://image2', 'https://image3'],
