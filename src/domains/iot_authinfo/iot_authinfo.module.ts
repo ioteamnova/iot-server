7ae1edd19a83627af8e44eb33290429612f43e33
humphrey909 <humphrey1858@gmail.com>
2023-05-06 08:36:20 +0900

    보드 리스트 pet조인해서 불러오기

--- src/domains/iot_authinfo/iot_authinfo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IotAuthinfoController } from './iot_authinfo.controller';
-import { IotAuthinfoService } from './iot_authinfo.service';
-
-@Module({
-  controllers: [IotAuthinfoController],
-  providers: [IotAuthinfoService]
-})
-export class IotAuthinfoModule {}
--- src/domains/iot_personal/iot_personal.module.ts
+++ src/domains/iot_personal/iot_personal.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { IotPersonalController } from './iot_personal.controller';
 import { IotPersonalService } from './iot_personal.service';
+import { Iot_personal } from './entities/iot_personal.entity';
+import { IotPersonalRepository } from './repositories/iot_personal.repository';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmExModule } from 'src/core/typeorm-ex.module';
 
 @Module({
+  imports: [
+    TypeOrmExModule.forCustomRepository([IotPersonalRepository]),
+    TypeOrmModule.forFeature(
+        [IotPersonalRepository],
+    )
+],
   controllers: [IotPersonalController],
   providers: [IotPersonalService]
 })
