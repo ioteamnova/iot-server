4b5d13d173dafe1a5ceb05aef353d943c9b2f91c
Jaden <yjp9603@gmail.com>
2023-05-08 03:20:46 +0900

    Fix: 스케줄 반복 프로퍼티 디버그

--- src/domains/schedule/entities/schedule.entity.ts
+++ src/domains/schedule/entities/schedule.entity.ts
@@ -1,3 +1,4 @@
+import { CreateScheduleDto } from './../dtos/create-schedule.dto';
 import { User } from 'src/domains/user/entities/user.entity';
 import BaseEntity from 'src/core/entity/base.entity';
 import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
@@ -25,28 +26,18 @@ export class Schedule extends BaseEntity {
     comment: `월~일요일을 배열에서 1과 0으로 표현한다.
     ex) 월,수,금 반복인경우 [1,0,1,0,1,0,0]`,
   })
-  repeat: number[];
+  repeat: string;
 
   @ManyToOne(() => User, (user) => user.schedules)
   @JoinColumn({ name: 'user_idx' })
   user: User;
 
-  static from({
-    title,
-    memo,
-    alarmTime,
-    repeat,
-  }: {
-    title: string;
-    memo: string;
-    alarmTime: Date;
-    repeat: number[];
-  }) {
+  static from(dto: CreateScheduleDto) {
     const schedule = new Schedule();
-    schedule.title = title;
-    schedule.memo = memo;
-    schedule.alarmTime = alarmTime;
-    schedule.repeat = repeat;
+    schedule.title = dto.title;
+    schedule.memo = dto.memo;
+    schedule.alarmTime = dto.alarmTime;
+    schedule.repeat = JSON.stringify(dto.repeat);
 
     return schedule;
   }
@@ -55,6 +46,6 @@ export class Schedule extends BaseEntity {
     this.title = dto.title;
     this.memo = dto.memo;
     this.alarmTime = dto.alarmTime;
-    this.repeat = dto.repeat;
+    this.repeat = JSON.stringify(dto.repeat);
   }
 }
--- src/domains/schedule/schedule.module.ts
+++ src/domains/schedule/schedule.module.ts
@@ -1,3 +1,4 @@
+import { UserRepository } from './../user/repositories/user.repository';
 import { ScheduleRepository } from './repositories/schedule.repository';
 import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
@@ -5,7 +6,9 @@ import { ScheduleController } from './schedule.controller';
 import { TypeOrmExModule } from 'src/core/typeorm-ex.module';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([ScheduleRepository])],
+  imports: [
+    TypeOrmExModule.forCustomRepository([ScheduleRepository, UserRepository]),
+  ],
   controllers: [ScheduleController],
   providers: [ScheduleService],
   exports: [TypeOrmExModule],
