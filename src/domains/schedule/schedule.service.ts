30725867b489214fe92f266cacea676e0d1db972
Jaden <yjp9603@gmail.com>
2023-05-21 18:55:18 +0900

    Fix: 깃헙에서 파이어베이스 json 파일 삭제

--- src/domains/schedule/schedule.service.ts
+++ src/domains/schedule/schedule.service.ts
@@ -8,12 +8,13 @@ import { UserRepository } from '../user/repositories/user.repository';
 import { HttpErrorConstants } from 'src/core/http/http-error-objects';
 import { ScheduleListDto } from './dtos/schedule-list.dto';
 import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import DateUtils from 'src/utils/date-utils';
 
 @Injectable()
 export class ScheduleService {
-  // private fcm: admin.messaging.Messaging;
+  private fcm: admin.messaging.Messaging;
 
   constructor(
     private scheduleRepository: ScheduleRepository,
@@ -21,10 +22,16 @@ export class ScheduleService {
   ) {
     // // eslint-disable-next-line @typescript-eslint/no-var-requires
     // const serviceAccount = require('../../../firebase-adminsdk.json');
-    // admin.initializeApp({
-    //   credential: admin.credential.cert(serviceAccount),
-    // });
-    // this.fcm = admin.messaging();
+    const serviceAccount: ServiceAccount = {
+      projectId: process.env.FB_PROJECT_ID,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    };
+    console.log(serviceAccount.privateKey);
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    this.fcm = admin.messaging();
   }
   async create(dto: CreateScheduleDto, userIdx: number) {
     const user = await this.userRepository.findByUserIdx(userIdx);
@@ -77,82 +84,82 @@ export class ScheduleService {
     await this.scheduleRepository.softDelete(scheduleIdx);
   }
 
-  // @Cron(CronExpression.EVERY_MINUTE)
-  // async sendPushMessages() {
-  //   const currentTime = DateUtils.momentTime();
-  //   console.log('currentTime::', currentTime);
-  //   // const testTime = '18:00'; // 테스트용
-  //   const day = DateUtils.momentDay();
+  @Cron(CronExpression.EVERY_MINUTE)
+  async sendPushMessages() {
+    const currentTime = DateUtils.momentTime();
+    console.log('currentTime::', currentTime);
+    const testTime = '18:00'; // 테스트용
+    const day = DateUtils.momentDay();
 
-  //   const schedules = await this.scheduleRepository.findSchedulesByTime(
-  //     currentTime,
-  //   );
-  //   if (schedules.length === 0) {
-  //     console.log('No schedules to send alerts.');
-  //     return;
-  //   }
+    const schedules = await this.scheduleRepository.findSchedulesByTime(
+      testTime,
+    );
+    if (schedules.length === 0) {
+      console.log('No schedules to send alerts.');
+      return;
+    }
 
-  //   const matchingSchedules = schedules.filter((schedule) => {
-  //     const repeatArray = schedule.repeat.split(',');
-  //     const isRepeat: boolean = repeatArray[day] === '1';
-  //     return isRepeat;
-  //   });
+    const matchingSchedules = schedules.filter((schedule) => {
+      const repeatArray = schedule.repeat.split(',');
+      const isRepeat: boolean = repeatArray[day] === '1';
+      return isRepeat;
+    });
 
-  //   if (matchingSchedules.length === 0) {
-  //     console.log('No matchingSchedules to send alerts.');
-  //     return;
-  //   }
-  //   const userTokensMap = new Map();
-  //   for (const matchingSchedule of matchingSchedules) {
-  //     const userToken = matchingSchedule.user.fbToken;
+    if (matchingSchedules.length === 0) {
+      console.log('No matchingSchedules to send alerts.');
+      return;
+    }
+    const userTokensMap = new Map();
+    for (const matchingSchedule of matchingSchedules) {
+      const userToken = matchingSchedule.user.fbToken;
 
-  //     if (!userTokensMap.has(userToken)) {
-  //       userTokensMap.set(userToken, []);
-  //     }
+      if (!userTokensMap.has(userToken)) {
+        userTokensMap.set(userToken, []);
+      }
 
-  //     userTokensMap.get(userToken).push(matchingSchedule);
-  //   }
+      userTokensMap.get(userToken).push(matchingSchedule);
+    }
 
-  //   for (const [userToken, userSchedules] of userTokensMap) {
-  //     const notifications = userSchedules.map((schedule) => {
-  //       return {
-  //         title: schedule.title,
-  //         body: schedule.memo,
-  //       };
-  //     });
+    for (const [userToken, userSchedules] of userTokensMap) {
+      const notifications = userSchedules.map((schedule) => {
+        return {
+          title: schedule.title,
+          body: schedule.memo,
+        };
+      });
 
-  //     console.log(
-  //       `${DateUtils.momentNow()} || Sending notifications to user with token: ${userToken}`,
-  //     );
-  //     await this.sendNotifications(notifications, userToken);
-  //   }
-  // }
+      console.log(
+        `${DateUtils.momentNow()} || Sending notifications to user: ${userSchedules}`,
+      );
+      await this.sendNotifications(notifications, userToken);
+    }
+  }
 
-  // async sendNotifications(notifications, token) {
-  //   try {
-  //     const responses = await Promise.all(
-  //       notifications.map(async (notification) => {
-  //         const message = {
-  //           notification: {
-  //             title: notification.title,
-  //             body: notification.body,
-  //           },
-  //           tokens: [token],
-  //           android: {
-  //             data: {},
-  //           },
-  //           apns: {
-  //             payload: {
-  //               aps: {},
-  //             },
-  //           },
-  //         };
-  //         return this.fcm.sendEachForMulticast(message);
-  //       }),
-  //     );
-  //     console.log('Successfully sent messages:', responses);
-  //   } catch (error) {
-  //     console.log('Error sending messages:', error);
-  //   }
-  // }
+  async sendNotifications(notifications, token) {
+    try {
+      const responses = await Promise.all(
+        notifications.map(async (notification) => {
+          const message = {
+            notification: {
+              title: notification.title,
+              body: notification.body,
+            },
+            tokens: [token],
+            android: {
+              data: {},
+            },
+            apns: {
+              payload: {
+                aps: {},
+              },
+            },
+          };
+          return this.fcm.sendEachForMulticast(message);
+        }),
+      );
+      console.log('Successfully sent messages:', responses);
+    } catch (error) {
+      console.log('Error sending messages:', error);
+    }
+  }
 }
