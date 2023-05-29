c032eb892de90da891ced65406dbfa25d6e78018
Jaden <yjp9603@gmail.com>
2023-05-29 20:44:37 +0900

    Fix: 스케줄러 기능 모두 주석 처리

--- src/domains/schedule/schedule.controller.ts
+++ src/domains/schedule/schedule.controller.ts
@@ -112,9 +112,9 @@ export class ScheduleController {
     return HttpResponse.ok(res);
   }
 
-  @Post('/send')
-  async sendNotification(@Res() res) {
-    const result = await this.scheduleService.sendPushMessages();
-    return HttpResponse.ok(res, result);
-  }
+  // @Post('/send')
+  // async sendNotification(@Res() res) {
+  //   const result = await this.scheduleService.sendPushMessages();
+  //   return HttpResponse.ok(res, result);
+  // }
 }
