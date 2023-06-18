d45f916ddbf1f0a102c3071cfff982561f4858ab
wngh4628 <wngh4628@naver.com>
2023-06-18 17:07:27 +0900

    sharp 기능 삭제

--- src/utils/s3-utils.ts
+++ src/utils/s3-utils.ts
@@ -4,7 +4,7 @@ import { ManagedUpload } from 'aws-sdk/lib/s3/managed_upload';
 import { logger } from './logger';
 import DateUtils from './date-utils';
 import * as uuid from 'uuid';
-import sharp from 'sharp';
+import * as sharp from 'sharp';
 
 export const s3 = new S3({
   accessKeyId: process.env.AWS_ACECSS_KEY_ID,
@@ -19,7 +19,7 @@ export const asyncUploadToS3 = async (
   if (isPNG(file)) {
     const originalExtension = fileKey.split('.').pop(); // 파일의 원래 확장자 추출
     fileKey = fileKey.replace(`.${originalExtension}`, '.jpeg'); // 확장자 변경
-    file = await convertToJPEG(file);
+    //file = await convertToJPEG(file);
   }
   const bucket = process.env.AWS_BUCKET_NAME;
   const s3 = new S3({
@@ -65,10 +65,10 @@ export const mediaUpload = async (
   return result.Location;
 };
 
-async function convertToJPEG(pngBuffer: Buffer): Promise<Buffer> {
-  const convertedImage = await sharp(pngBuffer).jpeg().toBuffer();
-  return convertedImage;
-}
+// async function convertToJPEG(pngBuffer: Buffer): Promise<Buffer> {
+//   const convertedImage = await sharp(pngBuffer).jpeg().toBuffer();
+//   return convertedImage;
+// }
 function isPNG(file: Buffer): boolean {
   // 파일 시그니처를 검사하여 PNG 파일인지 확인합니다.
   // 예를 들어, PNG 파일 시그니처는 '89 50 4E 47 0D 0A 1A 0A'입니다.
