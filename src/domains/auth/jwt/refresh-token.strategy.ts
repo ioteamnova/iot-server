7102f60e28bbea2c80e4a665156c1469828c004b
Jaden <yjp9603@gmail.com>
2023-06-18 19:56:28 +0900

    Feat: 리프레시 토큰 작업 (진행중)

--- src/domains/auth/jwt/refresh-token.strategy.ts
+++ src/domains/auth/jwt/refresh-token.strategy.ts
@@ -4,28 +4,28 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserRepository } from 'src/domains/user/repositories/user.repository';
 
-// @Injectable()
-// export class RefreshTokenStrategy extends PassportStrategy(
-//   Strategy,
-//   'jwt-refresh',
-// ) {
-//   constructor(private userRepository: UserRepository) {
-//     super({
-//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: process.env.JWT_SECRET,
-//       passReqToCallback: true,
-//     });
-//   }
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private userRepository: UserRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+      passReqToCallback: true,
+    });
+  }
 
-//   async validate(req: Request, payload: any) {
-//     const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
-//     console.log('JWT RefreshToken Strategy!!', refreshToken);
-//     const user = await this.userRepository.findOne({
-//       where: {
-//         idx: payload.userIdx,
-//         refreshToken: refreshToken,
-//       },
-//     });
-//     return user;
-//   }
-// }
+  async validate(req: Request, payload: any) {
+    const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+    console.log('JWT RefreshToken Strategy!!', refreshToken);
+    const user = await this.userRepository.findOne({
+      where: {
+        idx: payload.userIdx,
+        refreshToken: refreshToken,
+      },
+    });
+    return user;
+  }
+}
