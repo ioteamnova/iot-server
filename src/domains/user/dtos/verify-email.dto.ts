fb2be54988e4c29f63959e400476ea2a62d25086
Jaden <yjp9603@gmail.com>
2023-06-19 17:35:56 +0900

    Feat: 이메일 인증시 신규유저와 기존유저를 타입으로 구분하도록 추가

--- src/domains/user/dtos/verify-email.dto.ts
+++ src/domains/user/dtos/verify-email.dto.ts
@@ -1,7 +1,17 @@
 import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
+import { EmailVerifyType } from '../helper/constant';
 import { CreateUserDto } from './create-user.dto';
 
 export class VerifyEmailDto extends PickType(CreateUserDto, [
   'email',
-] as const) {}
+] as const) {
+  @ApiProperty({
+    description: '이메일 인증 유형',
+    required: true,
+    enum: EmailVerifyType,
+    default: EmailVerifyType.NEWUSER,
+  })
+  @IsNotEmpty()
+  type: EmailVerifyType;
+}
---
+++ src/domains/user/helper/constant.ts
@@ -0,0 +1,5 @@
+// 유저 권한
+export enum EmailVerifyType {
+  NEWUSER = 'NEWUSER',
+  OLDUSER = 'OLDUSER',
+}
