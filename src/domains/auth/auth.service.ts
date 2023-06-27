9408082c69066ebaf2bfa33cb753e9c45a2c80ba
Jaden <yjp9603@gmail.com>
2023-06-27 20:54:35 +0900

    Fix: JWT 토큰 만료 기간 원복

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -168,7 +168,7 @@ export class AuthService {
     const payload = { userIdx: userIdx };
     return this.jwtService.sign(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: '60s',
+      expiresIn: '2h',
     });
   }
 
@@ -176,7 +176,7 @@ export class AuthService {
     const payload = { userIdx: userIdx };
     return this.jwtService.sign(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: '120s',
+      expiresIn: '14 days',
     });
   }
 
