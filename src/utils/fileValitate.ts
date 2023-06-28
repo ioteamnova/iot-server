5e036cee654616852040ffc11012da4269f6e720
문준호 <wngh46280@gmail.com>
2023-06-28 21:44:51 +0900

    Revert "hls 테스트"
    This reverts commit b9540e6948ea002837604c61bb2ce08a0ec1cf4a.

--- src/utils/fileValitate.ts
+++ src/utils/fileValitate.ts
@@ -3,7 +3,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 export function fileValidates(files: Express.Multer.File[]) {
   if (files && files.length > 0) {
     const allowedFileTypes = /(?:png|jpeg|mov|mp4)$/i;
-    const maxFileSize = 200 * 1024 * 1024; // 200MB
+    const maxFileSize = 200 * 1024 * 1024; // 100MB
 
     for (const file of files) {
       if (!allowedFileTypes.test(file.originalname)) {
@@ -22,7 +22,7 @@ export function fileValidates(files: Express.Multer.File[]) {
 export function fileValidate(file: Express.Multer.File) {
   if (file) {
     const allowedFileTypes = /(?:png|jpeg|mov|mp4)$/i;
-    const maxFileSize = 200 * 1024 * 1024; // 200MB
+    const maxFileSize = 200 * 1024 * 1024; // 100MB
     if (!allowedFileTypes.test(file.originalname)) {
       throw new HttpException('Invalid file type', HttpStatus.BAD_REQUEST);
     }
