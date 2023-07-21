91692f2c01bd05b9138c9dc31634aa273ccbdb51
humphrey909 <humphrey1858@gmail.com>
2023-07-21 11:15:26 +0900

    test:live stream 저장 테스트

--- src/domains/live_stream/dtos/create-live-stream.dto.ts
+++ src/domains/live_stream/dtos/create-live-stream.dto.ts
@@ -1,29 +1,54 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-import { PageRequest } from 'src/core/page';
+// import { PageRequest } from 'src/core/page';
 
-export class CreateLiveStreamDto extends PageRequest {
+export class CreateLiveStreamDto {
+  @ApiProperty({
+    description: '게시글 고유번호',
+    default: '게시글 고유번호를 입력하는 내용 입니다. ',
+    required: true,
+  })
+  @IsNotEmpty()
+  boardIdx: number;
+
   @ApiProperty({
     description: '유저 고유번호',
-    default: '유저 고유번호를 입력하는 부분입니다. ',
+    default: '유저 고유번호를 입력하는 내용 입니다. ',
     required: true,
   })
   @IsNotEmpty()
   userIdx: number;
 
   @ApiProperty({
-    description: '보드 임시번호',
-    default: '사용자에게 보여줄 보드 임시번호 입니다.',
+    description: '최대 인원수',
+    default:
+      '최대 인원수를 입력하는 부분 입니다. 설정데이터가 아닌 방에 최대로 입장한 수를 기록합니다.',
     required: true,
   })
   @IsNotEmpty()
-  boardTempName: string;
+  maxNum: number;
 
   @ApiProperty({
-    description: '다이어리 내용',
-    default: '다이어리 내용입니다.',
+    description: '시작 시간',
+    default: '시작 시간을 기록하는 내용 입니다.',
     required: true,
   })
   @IsNotEmpty()
-  boardSerial: string;
+  startTime: Date;
+
+  @ApiProperty({
+    description: '종료 시간',
+    default: '종료 시간을 기록하는 내용 입니다.',
+    required: true,
+  })
+  @IsNotEmpty()
+  endTime: Date;
+
+  @ApiProperty({
+    description: '라이브 방송의 상태',
+    default: '라이브 방송의 상태를 기록하는 내용 입니다.',
+    required: true,
+  })
+  @IsNotEmpty()
+  state: number;
 }
--- src/domains/live_stream/entities/live-stream.entity.ts
+++ src/domains/live_stream/entities/live-stream.entity.ts
@@ -1,5 +1,6 @@
 import BaseEntity from 'src/core/entity/base.entity';
 import { Column, Entity } from 'typeorm';
+import { CreateLiveStreamDto } from '../dtos/create-live-stream.dto';
 
 @Entity()
 export class LiveStream extends BaseEntity {
@@ -25,13 +26,16 @@ export class LiveStream extends BaseEntity {
   endTime: Date;
 
   @Column()
-  status: number;
+  state: number;
 
-  // static fromDto(dto: CreateIotAuthDto) {
-  //   const iotAuthInfo = new IotAuthInfo();
-  //   iotAuthInfo.userIdx = dto.userIdx;
-  //   iotAuthInfo.boardTempName = dto.boardTempName;
-  //   iotAuthInfo.boardSerial = dto.boardSerial;
-  //   return iotAuthInfo;
-  // }
+  static fromDto(dto: CreateLiveStreamDto) {
+    const liveStream = new LiveStream();
+    liveStream.boardIdx = dto.boardIdx;
+    liveStream.userIdx = dto.userIdx;
+    liveStream.maxNum = dto.maxNum;
+    liveStream.startTime = dto.startTime;
+    liveStream.endTime = dto.endTime;
+    liveStream.state = dto.state;
+    return liveStream;
+  }
 }
