0c85fb13e89a63644c2289471807684db0fe9a64
humphrey909 <humphrey1858@gmail.com>
2023-07-21 10:09:24 +0900

    fix:경매 엔터티 추가, test:라이브스트림 테스트2

--- src/domains/live_stream/live_stream.module.ts
+++ src/domains/live_stream/live_stream.module.ts
@@ -3,9 +3,15 @@ import { LiveStreamController } from './live_stream.controller';
 import { LiveStreamService } from './live_stream.service';
 import { TypeOrmExModule } from 'src/core/typeorm-ex.module';
 import { LiveStreamRepository } from './repositories/live-stream.repository';
+import { BoardActionRepository } from '../board/repositories/board-action.repository';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([LiveStreamRepository])],
+  imports: [
+    TypeOrmExModule.forCustomRepository([
+      LiveStreamRepository,
+      BoardActionRepository,
+    ]),
+  ],
   controllers: [LiveStreamController],
   providers: [LiveStreamService],
 })
