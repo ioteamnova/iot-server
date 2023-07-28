bec43b534c8a6ec191cd27823aaaf2fa071af5e0
humphrey909 <humphrey1858@gmail.com>
2023-07-28 11:46:31 +0900

    comment:board 주석 정리

--- src/domains/board/board.service.ts
+++ src/domains/board/board.service.ts
@@ -29,11 +29,8 @@ import { BoardListDto } from './dtos/board-list.dto';
 import { fileValidate, fileValidates } from 'src/utils/fileValitate';
 import { DataSource, QueryRunner } from 'typeorm';
 import { RedisService } from '@liaoliaots/nestjs-redis';
-import { StreamKeyDto } from './dtos/steam-key.dto';
 import { BoardActionRepository } from '../board/repositories/board-action.repository';
-import { UpdateLiveStartTimeDto } from './dtos/update-live-start-time.dto';
 import { BoardAction } from './entities/board-action.entity';
-import { UpdateLiveEndTimeDto } from './dtos/update-live-end-time.dto';
 import { BoardCategoryPageRequest } from './dtos/board-category-page';
 import { LiveStreamRepository } from '../live_stream/repositories/live-stream.repository';
 
@@ -674,64 +671,4 @@ export class BoardService {
       return false;
     }
   }
-
-  // /**
-  //  *  라이브 스트리밍 정보 set
-  //  * @param dto 라이브 스트리밍 start, end 변경 / 전 경매방에 스트림키가 존재하는지 체크
-  //  * @returns StreamKeyDto
-  //  */
-  // async setLiveStreamInfo(val: string, dto: StreamKeyDto) {
-  //   //스트리밍이 끝났을 때 실행할 함수
-  //   //1. 키 형식 체크 : 맞으면 통과, 틀리면 false
-  //   //2. 경매 리스트에 스트림 키가 있는지 검토 : 있으면 통과, 없으면 false
-  //   //3. 통과 -> liveStream table에 저장 : action에 관련된 정보는 action에서 가져옴, 시작 시간 저장
-
-  //   const stream_from_chk = this.boardActionRepository.checkStreamKeyForm(
-  //     dto.name,
-  //   );
-
-  //   console.log('stream_from_chk');
-  //   console.log(stream_from_chk);
-
-  //   if (stream_from_chk) {
-  //     const actionInfo = await this.boardActionRepository.findOne({
-  //       where: {
-  //         streamKey: dto.name,
-  //       },
-  //     });
-
-  //     if (actionInfo) {
-  //       if (val == 'start') {
-  //         const liveStreamData: UpdateLiveStartTimeDto = {
-  //           liveStartTime: new Date(),
-  //           liveEndTime: null,
-  //           liveState: 1,
-  //         };
-
-  //         //update
-  //         actionInfo.updateStartFromDto(liveStreamData);
-  //         const result = await this.boardActionRepository.save(actionInfo);
-  //         return result;
-  //       } else {
-  //         //end
-  //         const liveStreamData: UpdateLiveEndTimeDto = {
-  //           liveEndTime: new Date(),
-  //           liveState: 0,
-  //         };
-
-  //         actionInfo.updateEndFromDto(liveStreamData);
-  //         const result = await this.boardActionRepository.save(actionInfo);
-  //         return result;
-  //       }
-  //     } else {
-  //       throw new UnauthorizedException(
-  //         HttpErrorConstants.LIVESTREAMINFO_NOT_EXIST,
-  //       );
-  //     }
-  //   } else {
-  //     throw new UnauthorizedException(
-  //       HttpErrorConstants.LIVESTREAMINFO_NOT_EXIST,
-  //     );
-  //   }
-  // }
 }
--- src/domains/board/repositories/board-action.repository.ts
+++ src/domains/board/repositories/board-action.repository.ts
@@ -20,23 +20,22 @@ export class BoardActionRepository extends Repository<BoardAction> {
   //       .getManyAndCount()
   //   );
   // }
-
-  checkStreamKeyForm(streamKey: string): boolean {
-    //형식 체크 RY1G-TzOv-lPKB-zRLO-sXI3
-    const streamKey_arr = streamKey.split('-');
-    console.log(streamKey_arr.length);
-    if (streamKey_arr.length == 5) {
-      for (let i = 0; i < streamKey_arr.length; i++) {
-        console.log(streamKey_arr[i].length);
-        if (streamKey_arr[i].length != 4) {
-          // return false;
-          throw new UnauthorizedException(HttpErrorConstants.INVALID_AUTH);
-        }
-      }
-    } else {
-      throw new UnauthorizedException(HttpErrorConstants.INVALID_AUTH);
-      // return false;
-    }
-    return true;
-  }
+  // checkStreamKeyForm(streamKey: string): boolean {
+  //   //형식 체크 RY1G-TzOv-lPKB-zRLO-sXI3
+  //   const streamKey_arr = streamKey.split('-');
+  //   console.log(streamKey_arr.length);
+  //   if (streamKey_arr.length == 5) {
+  //     for (let i = 0; i < streamKey_arr.length; i++) {
+  //       console.log(streamKey_arr[i].length);
+  //       if (streamKey_arr[i].length != 4) {
+  //         // return false;
+  //         throw new UnauthorizedException(HttpErrorConstants.INVALID_AUTH);
+  //       }
+  //     }
+  //   } else {
+  //     throw new UnauthorizedException(HttpErrorConstants.INVALID_AUTH);
+  //     // return false;
+  //   }
+  //   return true;
+  // }
 }
